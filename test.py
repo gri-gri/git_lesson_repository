a361c233ece0d90132060cb32cddc9e72c944bfb
Gri-Gri <gri_simonov@mail.ru>
2019-01-03 11:16:52 +0300

    Last_fantasy

--- test.py
+++ test.py
@@ -1,13 +1,4 @@
 import xlwt
 from datetime import datetime
-
-style0 = xlwt.easyxf('font: name Times New Roman, color-index red, bold on',
-    num_format_str='#,##0.00')
-sfejsgjldvln dsmvp;
-style1 = xlwt.easyxf(num_format_str='D-MMM-YY')
-
-wb = xlwt.Workbook()
-ws = wb.add_sheet('A Test Sheet')
-brance code
-let add another code
-bracnh code
+there wqas nothjing ro write
+so i wrote this
