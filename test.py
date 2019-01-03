83304431fb1950a36639fe08a80c682ad046ae78
Gri-Gri <gri_simonov@mail.ru>
2019-01-03 11:14:59 +0300

    Somethimg_new

--- test.py
+++ test.py
@@ -1,12 +1,15 @@
 import xlwt
 from datetime import datetime
-
-style0 = xlwt.easyxf('font: name Times New Roman, color-index red, bold on',
-    num_format_str='#,##0.00')
-sfejsgjldvln dsmvp;
-style1 = xlwt.easyxf(num_format_str='D-MMM-YY')
-
-wb = xlwt.Workbook()
-ws = wb.add_sheet('A Test Sheet')
-
-let add another code
+acdvas
+asdvijoif;oweifj
+sf
+sjaskdbvouehslkfnhxlozhcviowencfldxsuiohfenasl
+fesdfjkvfhkedvolvgwe[vasd
+                     svebnaljcvlacs
+                     acasnhclas
+                     casc
+                     scasa]asc
+as
+cas
+cajs;lcxnkzawlnxac
+cssjncllscnk
